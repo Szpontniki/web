3bbf726aa3d9add8c425b016879ffc15a66fd9b6
Azteczek <243776410+Azteczek@users.noreply.github.com>
2026-09-19 20:57:32 +0200

    Fixed file structure, added login files for the future

---
+++ flashinglights/src/Pages/Login/Login.tsx
@@ -0,0 +1,13 @@
+import './Login.css'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import z from 'zod'
+
+
+export default function Login() {
+    return (
+        <>
+
+        </>
+    )
+}
