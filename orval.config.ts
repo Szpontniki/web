f09842380eebf4f10c00522ce3b9eb0c61169007
Khenzii <khenzii@khenzii.dev>
2026-09-24 22:28:24 +0200

    feat: move code out of the `flashinglights` folder

--- orval.config.ts
+++ orval.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "orval";
 export default defineConfig({
   api: {
     input: {
-      target: "../api-schema/openapi.yaml",
+      target: "./api-schema/openapi.yaml",
       parserOptions: {
         externalRefs: {
           allow: ["*"],
